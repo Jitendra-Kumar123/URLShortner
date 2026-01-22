48722421635202756b2d63f219ce194135ed3bd0
Jitendra Kumar <jitendra0202006@gmail.com>
2026-01-22 21:05:20 +0530

    made changes in usercontroller

--- Backend/src/controller/user.controller.js
+++ Backend/src/controller/user.controller.js
@@ -1,8 +1,8 @@
 import wrapAsync from "../utils/tryCatchWrapper.js";
-import {getAllUserUrls} from "../dao/user.dao.js"
+import {getAllUserUrlsDao} from "../dao/user.dao.js"
 
 export const getAllUserUrls = wrapAsync(async(req, res) => {
     const {_id} = req.user
-    const urls = await getAllUserUrls(_id.toString())
+    const urls = await getAllUserUrlsDao(_id.toString())
     res.status(200).json({message: "success", urls})
 })
